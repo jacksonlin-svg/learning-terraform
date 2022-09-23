4bf6da55a7ec55da26a685a7fdd882dc5ac3419a
root <root@ip-172-31-13-41.ap-northeast-2.compute.internal>
2022-09-23 14:38:26 +0000

    initial commit

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c76973fbe0ee100c"
+  instance_type = "t2.micro"
+}
+
